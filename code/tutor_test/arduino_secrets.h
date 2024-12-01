71dcbf03a8eb416c75f42b6671d2da00f4d9772d
XIA JIN MING <ucbvjx4@ucl.ac.uk>
2024-12-01 22:03:13 +0000

    Add files via upload
    examples

---
+++ code/tutor_test/arduino_secrets.h
@@ -0,0 +1,8 @@
+// #define SECRET_SSID "CE-Hub-Student"
+// #define SECRET_PASS "casa-ce-gagarin-public-service"
+
+#define SECRET_SSID "water"
+#define SECRET_PASS "18986516618XJM/"
+
+#define SECRET_MQTTUSER "student"
+#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"

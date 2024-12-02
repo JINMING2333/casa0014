6f410f5b5a234cc0b0265e47937ff6b9147ee1d0
XIA JIN MING <ucbvjx4@ucl.ac.uk>
2024-12-02 10:15:09 +0000

    Delete code/beat_temp_display_now/beat_temp_display/arduino_secrets.h

--- code/beat_temp_display_now/beat_temp_display/arduino_secrets.h
+++
@@ -1,8 +0,0 @@
-#define SECRET_SSID "water"
-#define SECRET_PASS "18986516618XJM/"
-
-// #define SECRET_SSID "CE-Hub-Student"
-// #define SECRET_PASS "casa-ce-gagarin-public-service"
-
-#define SECRET_MQTTUSER "student"
-#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"
